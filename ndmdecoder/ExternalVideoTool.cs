84af1c91bbb49d74134e747189e436562178d0a7
HarpyWar <harpywar@gmail.com>
2014-10-15 10:49:14 +0400

    add ndmdecoder project

---
+++ ndmdecoder/ExternalVideoTool.cs
@@ -0,0 +1,115 @@
+﻿using Helper;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ndmdecoder
+{
+    class ExternalVideoTool
+    {
+
+        private Process _process;
+
+        private int roundNumber = 0;
+        private string outFile;
+        public ExternalVideoTool(string outFile)
+        {
+            this.outFile = outFile;
+        }
+
+
+        /// <summary>
+        /// Starts external video capture program
+        /// </summary>
+        private bool externalToolStart(string infile, int roundNumber)
+        {
+            this.roundNumber = roundNumber;
+
+            // start external tool
+            _process = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = Config.Data.ExternalToolRoundTrip[roundNumber].FileName,
+                    WorkingDirectory = Path.GetDirectoryName(Config.Data.ExternalToolRoundTrip[roundNumber].FileName),
+                    Arguments = Config.Data.ExternalToolRoundTrip[roundNumber].Args
+                        .Replace("{infile}", infile)
+                        .Replace("{outfile}", this.outFile),
+
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            Log.Info("Starting " + Config.Data.ExternalToolRoundTrip[roundNumber].FileName + " " + _process.StartInfo.Arguments);
+            _process.EnableRaisingEvents = true;
+            _process.Start();
+
+            // delay before set affinity + priority
+            Thread.Sleep(10000);
+            if (!_process.HasExited)
+            {
+                var affinity = int.Parse(Config.Data.ExternalToolRoundTrip[roundNumber].ProcessorAffinity);
+                if (affinity > 0)
+                    _process.ProcessorAffinity = (IntPtr)affinity;
+
+                _process.PriorityClass = (ProcessPriorityClass)Config.Data.ExternalToolRoundTrip[roundNumber].ProcessorPriority;
+            }
+
+            // wait for process exit
+            while (!_process.HasExited)
+            {
+                Application.DoEvents();
+                Thread.Sleep(100);
+            }
+            Log.Info("Round is completed.");
+
+            // check last round
+            if (this.roundNumber == Config.Data.ExternalToolRoundTrip.Length - 1)
+                return endMovieCreation(); 
+            else
+                return StartNextRound();
+        }
+
+
+        public bool StartNextRound()
+        {
+            // check is movie file created or size < 1MB
+            if (!File.Exists(this.outFile) || new FileInfo(this.outFile).Length < (1024 * 1024))
+            {
+                Log.Info("Movie file does not exist " + this.outFile);
+                return false;
+            }
+
+            // rename result file into (path + "input" + ext)
+            var infile = Path.Combine(Path.GetDirectoryName(this.outFile), "input" + Path.GetExtension(this.outFile));
+            if (File.Exists(infile))
+                File.Delete(infile);
+            Log.Info(string.Format("Rename {0} -> {1}", this.outFile, infile));
+            File.Move(this.outFile, infile);
+
+            this.roundNumber++;
+            // now start concat tool (intro file + result file)
+            return new ExternalVideoTool(this.outFile).externalToolStart(infile, this.roundNumber);
+        }
+
+
+        bool endMovieCreation()
+        {
+            // check is movie file created or size < 1MB
+            if (!File.Exists(this.outFile) || new FileInfo(this.outFile).Length < (1024 * 1024))
+            {
+                Log.Error("Movie file does not exist " + this.outFile);
+                return false;
+            }
+
+            Log.Info("Well done!");
+            return true;
+        }
+
+
+    }
+}
---
+++ ndmdecoder/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Helper;
+using System.IO;
+
+namespace ndmdecoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Visible = false;
+
+            Config.Load();
+
+            var files = Directory.GetFiles(Config.Data.TempDir, "*.json");
+            if (files.Length == 0)
+                Environment.Exit(0);
+
+            try
+            {
+
+                string jsonFile = null;
+                for (int j = 0; j < files.Length; j++)
+                {
+                    if (Common.IsFileLocked(new FileInfo(files[j])))
+                        continue;
+
+                    jsonFile = files[j];
+
+                    // if demo already completed and waiting for upload
+                    var d = Common.ReadJsonDemo(File.ReadAllText(jsonFile));
+                    if (d.local_completed)
+                    {
+                        jsonFile = null;
+                        continue;
+                    }
+
+                    break;
+                }
+                if (jsonFile == null)
+                    Environment.Exit(0);
+
+                Log.Info("Reading demo info file " + jsonFile);
+
+                bool result = false;
+                DemoItem demo;
+                // lock file while processing
+                using (var fs = new FileStream(jsonFile, FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    using (var sr = new StreamReader(fs, Encoding.UTF8))
+                    {
+                        // open first available json file with demo info
+                        demo = Common.ReadJsonDemo(sr.ReadToEnd());
+                        result = processDemo(demo);
+                    }
+                }
+                if (result)
+                {
+                    // set complete flag
+                    demo.local_completed = true;
+                    // save with modified flag back to json
+                    Common.SaveJsonDemo(jsonFile, demo);
+                }
+                // if something wrong then remove json
+                else
+                {
+                    Log.Info("Consistently is wrong. Delete " + jsonFile);
+                    File.Delete(jsonFile);
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                Log.Error(e.StackTrace);
+            }
+
+            Environment.Exit(0);
+
+        }
+
+        private bool processDemo(DemoItem demo)
+        {
+            // if external tool not used 
+            if (!Config.Data.ExternalVideoCapture)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < demo.local_videos.Length; i++ )
+            {
+                var fileName = demo.local_videos[i].FileName;
+                // it's assumed that video file exists
+
+                var result = new ExternalVideoTool(fileName)
+                    .StartNextRound(); // start with second round
+
+                if (!result)
+                    return false;
+
+                // stop if no follow player
+                if (demo.local_followplayer == false)
+                    break;
+            }
+            return true;
+        }
+    }
+}
